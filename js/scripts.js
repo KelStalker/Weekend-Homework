c613cb35f22ccd2c89ae3eea79de7578788b6adc
Kel <kelstalker@hotmail.com>
2020-03-29 17:35:03 -0600

    AOM challenge 'almost' working

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
-var AOMform = {
-    form: document.querySelector(form),
-    input: document.querySelector(id = "userNumber"),
-    input: document.querySelector(id = "userExponent"),
-    input: document.querySelector("userExponent").querySelector,
-}
 
-function calculateAOM(userNumber, userExponent){
+//------- WHW Assignment 1.1 - Array Of Multitudes -------
 
+//console.log("Hello World!")
+
+var x = document.getElementById("userNumber")
+var y = document.getElementById("userExponent")
+
+// takes two arguments (above) and calculates as 'x to the power of y'
+function calculateAOM(x, y) {
+
+    // create an array for the output to exist in
+    var result = []
+
+    // for each iteration, multiply the current value of I with the userNumber
+    for (i = 1; i <= y; i++) {
+
+        // store each iteration consecutively in the reslut array created above
+        result.push(x * i)
+        console.log(result[i]);
+    }
 
 }

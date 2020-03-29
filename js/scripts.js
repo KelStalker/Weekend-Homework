9b35365b2b303d402d9f2eafac0444c1bc8d80f8
Kel <kelstalker@hotmail.com>
2020-03-29 15:23:32 -0600

    Form finished, need to output the  return array still

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var AOMform = {
+    form: document.querySelector(form),
+    input: document.querySelector(id = "userNumber"),
+    input: document.querySelector(id = "userExponent"),
+    input: document.querySelector("userExponent").querySelector,
+}
+
+function calculateAOM(userNumber, userExponent){
+
+
+}
